e6d79b6042f76e4bdcc564b08d27da1d9ac150cf
lijunchang <lijunchang@itixiao.com>
2023-11-06 18:57:12 +0800

    chore: sass

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -15,30 +15,28 @@ import { ref } from 'vue'
 const title = ref('Hello')
 </script>
 
-<style>
+<style lang="scss" scoped>
 .content {
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-}
+  .logo {
+    height: 200rpx;
+    width: 200rpx;
+    margin-top: 200rpx;
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: 50rpx;
+  }
+  .text-area {
+    display: flex;
+    justify-content: center;
+  }
 
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
+  .title {
+    font-size: 36rpx;
+    color: #8f8f94;
+  }
 }
 </style>
