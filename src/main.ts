3dffdee92ce9f88eb45b178a1b8a4ef781e043fd
lijunchang <lijunchang@itixiao.com>
2023-11-07 15:14:33 +0800

    chore: uview-plus

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createSSRApp } from 'vue'
+import uviewPlus from 'uview-plus'
 import App from './App.vue'
 import { setupStore } from '@/store'
 
@@ -6,6 +7,8 @@ export function createApp() {
   const app = createSSRApp(App)
 
   setupStore(app)
+
+  app.use(uviewPlus)
 
   return {
     app,
